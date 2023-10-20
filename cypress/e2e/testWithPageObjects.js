ef2d2f8b111e7f1b5140c5565d8ae16786fdb01b
LGTestspecialist <laraguis@testspecialist.nl>
2023-10-20 12:46:02 +0200

    changes

--- cypress/e2e/testWithPageObjects.js
+++ cypress/e2e/testWithPageObjects.js
@@ -1,9 +1,12 @@
 import { navigateTo } from "../support/page_objects/navigationPage"
+import { onFormLayoutsPage } from "../support/page_objects/formLayoutsPage"
+import { onDatePickerPage } from "../support/page_objects/datepickerPage"
+import { onSmartTablePage } from "../support/page_objects/smartTablePage"
 
-describe('Test with Page Objets', () => {
+describe('Test with Page Objects', () => {
 
     beforeEach('open application', () => {
-        cy.visit('/')
+        cy.openHomePage()
     })
 
     it('verify navigation across the pages', () => {
@@ -12,7 +15,20 @@ describe('Test with Page Objets', () => {
         navigateTo.toasterPage()
         navigateTo.smartTablePage()
         navigateTo.tooltipPage()
+        navigateTo.toasterPage()
     })
 
+        it.only(' should submit Inline and Basic form and select tomorrow date in the calendar', () => {
+            navigateTo.formLayoutsPage()
+            onFormLayoutsPage.submitInlineFormWithNameAndEmail('Artem','test@test.com')
+            onFormLayoutsPage.submitBasicFormWithEmailAndPassword('test@test.com', 'password')
+            navigateTo.datepickerPage()
+            onDatePickerPage.selectCommonDatepickerDateFromToday(1)
+            onDatePickerPage.selectDatepickerWithRangeFromToday(7, 14)
+            navigateTo.smartTablePage()
+            onSmartTablePage.addNewRecordWithFirstAndLastName('Lara', 'Guis')
+            onSmartTablePage.updateAgeByFirstName('Lara', '43')
+            onSmartTablePage.deleteRowByIndex(1)
+        })
 
 })
